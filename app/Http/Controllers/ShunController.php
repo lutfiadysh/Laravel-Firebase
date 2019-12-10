de101c1e90955402fb35de7ae706ced5e28e158c
lutfiadysh <lutfi020117@gmail.com>
2019-12-10 09:10:57 +0700

    laravel-firebase

---
+++ app/Http/Controllers/ShunController.php
@@ -0,0 +1,108 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Kreait\Firebase;
+use Kreait\Firebase\Factory;
+use Kreait\Firebase\ServiceAccount;
+use Kreait\Firebase\Database;
+
+class ShunController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $serviceAccount = ServiceAccount::fromJsonFile(__DIR__.'/qr-scanner-ee644-0ff51ab53c39.json');
+        $firebase = (new Factory)
+        ->withServiceAccount($serviceAccount)
+        ->withDatabaseUri('https://qr-scanner-ee644.firebaseio.com/')
+        ->create();
+        $database = $firebase->getDatabase();
+        $newPost = $database
+        ->getReference('data');
+        // ->push([
+        // 'title' => 'Post title',
+        // 'body' => 'This should probably be longer.'
+        // ]);
+
+        //$newPost->getKey(); // => -KVr5eu8gcTv7_AHb-3-
+        //$newPost->getUri(); // => https://my-project.firebaseio.com/blog/posts/-KVr5eu8gcTv7_AHb-3-
+        //$newPost->getChild('title')->set('Changed post title');
+        //$newPost->getValue(); // Fetches the data from the realtime database
+        //$newPost->remove();
+        echo"<pre>";
+        print_r($newPost->getvalue());
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
