312a1621d52376284c7873ad061cf81a78fad8d5
lutfiadysh <lutfi020117@gmail.com>
2020-01-03 15:26:39 +0700

    terakhir

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,7 +6,7 @@
     <div class="container mt--8 pb-5">
         <div class="row justify-content-center">
             <div class="col-lg-5 col-md-7">
-                <div class="card bg-secondary shadow border-0">
+                <div class="card bg-secondary shadow border-0 mt--6 p-4">
                     <div class="card-body px-lg-5 py-lg-5">
                         <div class="text-center text-muted mb-4">
                             <h3>Silahkan Login</h3>
